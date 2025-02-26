7c238444b422564a6d25387f79e706cc8a505bdf
Narayan Bhardwaj <narayan6701@outlook.in>
2025-02-26 13:17:13 +0530

    implemented some features

--- index.js
+++ index.js
@@ -2,45 +2,54 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
-
-app.use(express.urlencoded({extended:true}));
+const { v4: uuidv4 } = require('uuid');
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
-app.set("views",path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
-    {
-        username: "Narayan",
-        content: "I love coding"
-    },
-    {
-        username: "Kirti",
-        content: "I love maths"
-    },
-    {
-        username: "Payal",
-        content: "I love heheheehe"
-    },
+  {
+    id: uuidv4(),
+    username: "Narayan",
+    content: "I love coding",
+  },
+  {
+    id: uuidv4(),
+    username: "Kirti",
+    content: "I love maths",
+  },
+  {
+    id: uuidv4(),
+    username: "Payal",
+    content: "I love heheheehe",
+  },
 ];
 
-app.get("/posts", (req,res)=>{
-    res.render("index.ejs", {posts} )
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
 });
 
-app.get("/posts/new", (req,res)=>{
-    res.render("new.ejs");
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
 });
 
-app.post("/posts",(req,res)=>{
-    let {username, content} = req.body;
-    posts.push({username, content});
-    console.log(req.body);
-    res.send("Post request received");
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  let id = uuidv4();
+  posts.push({ id, username, content });
+  res.redirect("/posts");
 });
 
-app.listen(port, ()=> {
-    console.log(`Server is running on port ${port}`);
+app.get("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p)=>id===p.id);
+    // console.log(post);
+    // res.send("request working"); 
+    res.render("show.ejs",{post});
 });
 
-
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
