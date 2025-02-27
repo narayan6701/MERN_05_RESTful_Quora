80fc579fbd219c277e88bac6263517b0316c123f
Narayan Bhardwaj <narayan6701@outlook.in>
2025-02-27 11:39:19 +0530

    finally learnt RESTful API and CRUD operations

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+var methodOverride = require('method-override')
 const { v4: uuidv4 } = require('uuid');
 app.use(express.urlencoded({ extended: true }));
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride('_method'))
 
 let posts = [
   {
@@ -48,6 +51,27 @@ app.get("/posts/:id", (req, res) => {
     // console.log(post);
     // res.send("request working"); 
     res.render("show.ejs",{post});
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let {id} = req.params;
+  let newContent = req.body.content;
+  let post = posts.find((p)=>id===p.id);
+  post.content = newContent;
+  console.log(post);
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let {id} = req.params;
+  post = posts.filter((p)=>id!==p.id);
+  res.render("edit.ejs", {post});
+});
+
+app.delete("/posts/:id", (req, res) => {
+  let {id} = req.params;
+  let post = posts.find((p)=>id===p.id);
+  res.redirect("/posts");
 });
 
 app.listen(port, () => {
